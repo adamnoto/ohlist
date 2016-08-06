282fe63acc32ee5c97e4aba48ff4154dd6757d4c
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-08-06 17:08:37 +0700

    implementation detail for ruby version

--- rb/ohlist/lib/ohlist.rb
+++ rb/ohlist/lib/ohlist.rb
@@ -1,5 +1,26 @@
 require "ohlist/version"
 
 module Ohlist
-  # Your code goes here...
+  extend self
+
+  # func contains accumulator
+  def foldr(list, accumulator, func = nil)
+    acc = accumulator
+    list.each do |element|
+      if block_given?
+        acc = yield element, acc
+      else
+        acc = func.(element, acc)
+      end
+    end
+    acc
+  end
+
+  def foldl(list, accumulator, func = nil, &block)
+    if block_given?
+      foldr(list.reverse, accumulator, &block)
+    else
+      foldr(list.reverse, accumulator, func)
+    end
+  end
 end
