b088d376198b97db01b60bafadb0c3932001c997
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-08-07 23:33:46 +0700

    C implementation refactored for compacter code

--- c/ohlist/ext/ohlist/ohlist.c
+++ c/ohlist/ext/ohlist/ohlist.c
@@ -3,27 +3,28 @@
 static VALUE
 fold(int argNum, VALUE list, VALUE accumulator, VALUE lambda) {
   int listLen = RARRAY_LEN(list);
-  int i;
 
-  if (argNum == 2) {
-    // must declare a block, as no proc is given!
-    if (!rb_block_given_p()) {
-      rb_need_block();
+  // hold value of each iteration of element in the list
+  VALUE element = Qundef;
+  for(int i=0; i<listLen; i++) {
+    // get the element
+    element = rb_ary_entry(list, i);
+
+    if (argNum == 2) {
+      // raise block not given error if no block passed when
+      // given 2 arguments/parameters
+      if (!rb_block_given_p()) rb_need_block();
+      else
+        accumulator = rb_yield_values(2, element, accumulator);
+    } else if (argNum == 3) {
+      if (rb_class_of(lambda) != rb_cProc)
+        rb_raise(rb_eTypeError, "Expected Proc callback");
+
+      accumulator = rb_funcall(lambda, rb_intern("call"), 2,
+          element, accumulator);
     } else {
-      for(i = 0; i<listLen; i++) {
-        accumulator = rb_yield_values(2, rb_ary_entry(list, i), accumulator);
-      }
+      rb_raise(rb_eArgError, "Unexpected number of arguments. Accepts 2..3");
     }
-  } else if (argNum == 3) {
-    if (rb_class_of(lambda) != rb_cProc) {
-      rb_raise(rb_eTypeError, "Expected Proc callback");
-    }
-
-    for(i = 0; i<listLen; i++) {
-      accumulator = rb_funcall(lambda, rb_intern("call"), 2, rb_ary_entry(list, i), accumulator);
-    }
-  } else {
-    rb_raise(rb_eArgError, "Unexpected number of arguments. Accepts 2..3");
   }
 
   return accumulator;
